135d88563c4c51bb76c4d5cf46494e800482055e
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-02-17 12:47:38 +0300

    Solution to 15 problems

---
+++ Task 15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите порядковый номер дня недели >> ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+while (value > 7){
+    Console.WriteLine("В неделе всего 7 дней");
+    Console.WriteLine($"Возможно вы имели ввиду {value % 7}?");
+    Console.Write("Для подтверждения введите номер дня недели снова >> ");
+    value = Convert.ToInt32(Console.ReadLine());
+}
+
+string answer = "да";
+if (value < 6) {
+    answer = "нет";
+}
+
+Console.Write($"Введенный номер дня соответствует выходному (да / нет): {answer}");
