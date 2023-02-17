030db98ba57de8622f85aed79402795e105a1d6b
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-02-17 11:48:46 +0300

    Fixed a bug in Task 10

--- Task 10/Program.cs
+++ Task 10/Program.cs
@@ -1,3 +1,3 @@
-﻿Console.Write("Введите число  трехзначное число >> ");
+﻿Console.Write("Введите трехзначное число >> ");
 string value = Console.ReadLine();
 Console.Write($"У числа {value} вторая цифра {value[1]}");
