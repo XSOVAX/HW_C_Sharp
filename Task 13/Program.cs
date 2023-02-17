438bf7f5c2a57d35f32efbab39c111047444aad9
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-02-17 12:36:30 +0300

    Solution to 13 problems

---
+++ Task 13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число  >> ");
+string value = Console.ReadLine();
+if (value.Length < 3){
+    Console.Write("третьей цифры нет");
+}
+else{
+    Console.Write($"У числа {value} третья цифра {value[2]}");
+}
