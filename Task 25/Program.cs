165e3fa4ed77c91e767215b3e0db6ba3fab80d89
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-02-28 17:57:13 +0300

    Solution to 25 problems

---
+++ Task 25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число для возведения в степень >> ");
+int value = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень >> ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int answer = 1;
+while (degree != 0){
+    answer *= value;
+    degree--;
+}
+
+Console.Write($"{answer}");
