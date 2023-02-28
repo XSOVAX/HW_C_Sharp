4824e5089a7d332a2d3694d375d0dd7081edac1a
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-02-28 18:10:55 +0300

    Solution to 27 problems

---
+++ Task 27/Program.cs
@@ -0,0 +1,10 @@
+﻿int sum_number (int n){
+    if (n == 0) return 0;
+    return n % 10 + sum_number(n / 10);
+}
+
+
+Console.Write("Введите число >> ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{value} -> {sum_number(value)}");
