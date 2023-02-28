aec41db1bf405208695da44d69b8ce8472ef9c5e
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-02-28 18:16:45 +0300

    Сorrected the solution to a solution with a function

--- Task 25/Program.cs
+++ Task 25/Program.cs
@@ -1,12 +1,13 @@
-﻿Console.Write("Введите число для возведения в степень >> ");
+﻿
+int value_degree (int n, int k){
+    if (k == 0) return 1;
+    if (k == 1) return n;
+    return n * value_degree(n, k - 1);
+}
+
+Console.Write("Введите число для возведения в степень >> ");
 int value = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите степень >> ");
 int degree = Convert.ToInt32(Console.ReadLine());
 
-int answer = 1;
-while (degree != 0){
-    answer *= value;
-    degree--;
-}
-
-Console.Write($"{answer}");
+Console.Write($"{value}, {degree} -> {value_degree(value, degree)}");
