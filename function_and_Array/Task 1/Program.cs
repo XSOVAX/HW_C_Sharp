8bb1bfb7c5c7ff1a18d490887cc25ef70e377ae4
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-03-01 10:52:53 +0300

    Solution function_and_Array to 1 problems

---
+++ function_and_Array/Task 1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int [] fill_random_array(int [] array, int min_value, int max_value){
+    int size_array = array.Length;
+    Random rnd = new Random();
+    for (int i = 0; i < size_array; i++){
+        array[i] = rnd.Next(min_value, max_value);
+    }
+    return array;
+}
+
+int [] print_array(int [] array){
+    int size_array = array.Length;
+    Console.Write($"[{array[0]}");
+    for (int i = 1; i < size_array; i++){
+        Console.Write($", {array[i]}");
+    }
+    Console.Write("]");
+    return array;
+}
+
+Console.Write("Введите размер массива >> ");
+int array_size = Convert.ToInt32(Console.ReadLine());
+int [] my_array = new int[array_size];
+my_array = fill_random_array(my_array, 100, 1000);
+
+int count = 0;
+for (int i = 0; i < my_array.Length; i++){
+    if (my_array[i] % 2 == 0) count++;
+}
+
+print_array(my_array);
+Console.Write(" -> ");
+Console.Write(count);
