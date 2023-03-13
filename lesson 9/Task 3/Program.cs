93456b9df239aac723eae15a0175fed8179c8342
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-03-13 14:17:27 +0300

    Solved 3 problem

---
+++ lesson 9/Task 3/Program.cs
@@ -0,0 +1,15 @@
+﻿int fun_Akkerman(int n, int m){
+    if (n == 0)
+        return m + 1;
+    else
+        if ((n != 0) && (m == 0))
+            return fun_Akkerman(n - 1, 1);
+        else
+            return fun_Akkerman(n - 1, fun_Akkerman(n, m - 1));
+}
+
+Console.Write("Введите значение M -> ");
+int value_m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N -> ");
+int value_n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"m = {value_m}, n = {value_n} -> A({value_m}, {value_n}) = {fun_Akkerman(value_m, value_n)}");
