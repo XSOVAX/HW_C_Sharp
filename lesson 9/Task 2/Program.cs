88ec5102219d14d1428b204a6055cb890ec201e6
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-03-13 14:09:46 +0300

    Solved 2 problem

---
+++ lesson 9/Task 2/Program.cs
@@ -0,0 +1,10 @@
+﻿int sum_numbers(int a, int b){
+    if(a > b) return 0;
+    return a + sum_numbers(a + 1, b);
+}
+
+Console.Write("Введите значение M -> ");
+int value_m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N -> ");
+int value_n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Сумма чисел в диапозоне от {value_m} до {value_n} -> {sum_numbers(value_m, value_n)}");
